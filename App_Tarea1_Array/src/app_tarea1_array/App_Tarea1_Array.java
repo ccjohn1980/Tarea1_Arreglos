0fe0a74994f7a489fb1525ae9a3bfc307522d1e9
Cindy John <ccjohn1980@gmail.com>
2020-02-14 14:29:43 -0500

    solucion de la tarea 1

--- App_Tarea1_Array/src/app_tarea1_array/App_Tarea1_Array.java
+++ App_Tarea1_Array/src/app_tarea1_array/App_Tarea1_Array.java
@@ -1,11 +1,28 @@
 package app_tarea1_array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class App_Tarea1_Array {
 
-   
     public static void main(String[] args) {
+        ArrayList<Integer> numbersList = new ArrayList<Integer>(Arrays.asList(1,4,5,5,4,2,1,3));
+        for(Integer number: numbersList){
+            ArrayList<Integer> tmp = (ArrayList)numbersList.clone();
+            tmp.remove(number);
+            if (!tmp.contains(number)){
+             System.out.println("el valor que no se repite es " + number + " \nindice del numero que no se repita es :" + numbersList.indexOf(number));
+             break;
+            }
+        }
+
+    }
+       
+       
+    
+        
         
     }
     
-}
+
